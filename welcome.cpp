e9029a637836f60c286327b9ebef6fdd93b6cc56
Demilition <67475784+Demilition@users.noreply.github.com>
2022-12-02 15:23:20 -0600

    Create welcome.cpp

---
+++ welcome.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "html++.h"
+  
+  int main()
+{
+  html style="color:red;"<
+    head<
+      title<"Help Me.">
+    >,
+    body<
+      h1<"The horror!">,
+      p<"Someone has probably done this before, but I can see why it didn't catch on.">,
+      a<"href=https://github.com/csb6/html-plus-plus", "For science">
+    >
+  > page;
+
+
+  std::cout << page.content;
+
+  return 0;
+  
+  
